00dc097267f034a601f7f8d39aa4f50abb65b8d4
Frozen12 <31291110+Frozen12@users.noreply.github.com>
2022-06-09 14:06:34 +0600

    Create new file

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+apt update && apt upgrade -y && apt install -y software-properties-common && apt update && add-apt-repository universe && add-apt-repository multiverse
+apt update && apt install -y --no-install-recommends \
+        php python3.8 python3-pip curl wget git nodejs npm \
+        make python-is-python3 build-essential cmake \
+        nano \
+        zip unzip p7zip-full rar unrar \
+        ffmpeg \
+        openssh-server \
+        rdfind
+
+rm -rf /var/lib/apt/lists/*
+npm install -g localtunnel yarn
+pip3 install --no-cache-dir -r requirements.txt
+
+# install aria2c
+# curl -Oaria2.deb https://tebi.com/7yhtraria2.deb
+
+
+# Install TGrclonebot
+git clone https://github.com/Frozen12/TelegramShellBot-Rclone.git
+cd TelegramShellBot-Rclone
+yarn install
+
