519c2ec2449c6c03dd1d73696b913f85df794d5e
Frozen12 <31291110+Frozen12@users.noreply.github.com>
2022-06-11 23:25:30 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,6 +22,6 @@ pip3 install --no-cache-dir -r requirements.txt
 
 # Install TGrclonebot
 git clone https://github.com/Frozen12/TelegramShellBot-Rclone.git
-# cd TelegramShellBot-Rclone
+cd TelegramShellBot-Rclone
 yarn install
 
