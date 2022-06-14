586ab0ab6fafb02396f05887960d9e8901c0f856
Frozen12 <31291110+Frozen12@users.noreply.github.com>
2022-06-15 04:14:48 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Install rclone static binary
+wget -q https://downloads.rclone.org/v1.58.1/rclone-v1.58.1-linux-amd64.zip
+unzip -q rclone-*-linux-amd64.zip
+export PATH=$PWD/rclone-v1.58.1-linux-amd64:$PATH
+echo "Rclone installed successfully"
+# remove junk
+rm -rf rclone-*-linux-amd64.zip *.txt *yml *.md
+
 # Create rclone.conf file from base64
 if [[ -n $RCLONE_CONFIG_BASE64 ]]; then
 	echo "Rclone config detected"
