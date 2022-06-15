7e2147047db054f2481a1f699501c567b4607df8
Frozen12 <31291110+Frozen12@users.noreply.github.com>
2022-06-16 03:21:33 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-# Create rclone.conf file from base64
-if [[ -n $RCLONE_CONFIG_BASE64 ]]; then
-	echo "Rclone config in BASE64 Format detected"
-	echo "[DRIVE]" > rclone.conf
-    mkdir -p /root/.config/rclone
-	echo "$(echo $RCLONE_CONFIG_BASE64|base64 -d)" >> /root/.config/rclone/rclone.conf
-fi
-
-# fetch rclone.conf from url
-
-if [[ -n $RCLONE_CONFIG_URL ]]; then
-	echo "Rclone config file url detected. Fetching rclone.conf . . ."
-	mkdir -p /root/.config/rclone
-    curl -o/root/.config/rclone/rclone.conf "$RCLONE_CONFIG_URL"
-
-fi
-
-# echo " Rclone Config File loaded successfully "
-
 # Set bot token & owner ID
 
 if [[ -n $BOT_TOKEN && -n $OWNER_ID ]]; then
